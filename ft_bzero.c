ca392633ba00dc052df3c6af1838f7b020006a7b
Mael Guilbert Costes <mguilber@3C6.42angouleme.fr>
2025-10-14 11:10:19 +0200

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,8 +1,22 @@
-void ft_bzero(void *s, size_t n){
-	
+void *ft_bzero(char *s, int n){
+	int i;
+
+	i = 0;
+	while(i <= n){
+		s[i] = '\0';
+		i++;
+	}
+	return(s);
 }
 #include <stdio.h>
 #include <string.h>
 int main(){
-	
+	char s[11] = "hello world";
+	char c[11] = "hola mundo";
+	printf("%s\n", s);
+	printf("%s\n", c);
+	bzero(s, 2);
+	ft_bzero(c, 7);
+	printf("%s\n", s);
+	printf("%s", c);
 }
