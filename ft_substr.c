27d7b7d45df7775397ceb0220d3d224c56d1f153
sudoDodo <guilbertmael@gmai.com>
2025-10-20 21:07:14 +0200

    cal

--- ft_substr.c
+++ ft_substr.c
@@ -1,27 +1,28 @@
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len){
-  int i;
-  int max;
-  char *str;
-  int j;
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int		i;
+	int		max;
+	char	*str;
+	int		j;
 
-  i = start;
-  max = len;
-  str = malloc(max - i);
-  j = 0;
-
-  while(i < max){
-    str[j] = s[i];
-    i++;
-    j++;
-  }
-  str[j] = '\0';
-  return (str);
+	i = start;
+	max = len;
+	str = malloc(max - i);
+	j = 0;
+	while (i < max)
+	{
+		str[j] = s[i];
+		i++;
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
 /*
-int main() {
+int	main(void) {
   printf("%s\n", ft_substr("i just want this part #############", 5, 20));
-  return 0;
+  return (0);
 }
 */
--- libft.h
+++ libft.h
@@ -1,11 +1,12 @@
 #ifndef LIBFT_H
 # define LIBFT_H
-#include <bsd/string.h>
 # include "ctype.h"
+# include <bsd/string.h>
 # include <stddef.h> // pour size_t
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <limits.h>
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -25,7 +26,8 @@ char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
 char	*ft_strrchr(const char *s, int c);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void *ft_calloc(size_t n, size_t size);
 
-char *ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 #endif
