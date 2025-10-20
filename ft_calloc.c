e09fb916136fc4cb1eccce1cbba611b0a7a454f6
sudoDodo <guilbertmael@gmai.com>
2025-10-20 21:08:56 +0200

    cal

--- ft_calloc.c
+++ ft_calloc.c
@@ -21,9 +21,11 @@ int main() {
 
  	void * d1 = ft_calloc(size, sizeof(int));
  	void * d2 = calloc(size, sizeof(int));
- 	if (memcmp(d1, d2, size * sizeof(int)))
+/*
+  if (memcmp(d1, d2, size * sizeof(int)))
     printf("%i\n", memcmp(d1, d2, size * sizeof(int)));
  	free(d1);
  	free(d2);
   printf("TEST_SUCCESS");
+*/
 }
