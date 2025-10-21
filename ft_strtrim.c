f555ce501573bf1db370a3a9746ef17371c3cbf7
sudoDodo <guilbertmael@gmai.com>
2025-10-21 14:50:49 +0200

    supp char on str

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,5 +1,23 @@
 #include "libft.h"
 
+char *ft_supp(char *str, int i){
+  while(str[i]){
+    str[i] = str[i +1];
+    i++;
+  }
+}
+
 char *ft_strtrim(char const *s1, char const *set){
-  
+  int i = 0;
+  int j = 0;
+  char *s2;
+  s2 = malloc(sizeof(s1));
+
+  while(s1[i]){
+    while(set[j]){
+      if (s1[i] == set[j]){
+        ft_supp()
+      }
+    }
+  }
 }
