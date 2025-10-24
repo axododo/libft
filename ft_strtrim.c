f4b5f5950e407334aed99eb90b4657256ced94a5
Mael Guilbert Costes <mguilber@1G6.42angouleme.fr>
2025-10-24 17:41:02 +0200

    trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,52 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mguilber <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mguilber <mguilber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 14:37:33 by mguilber          #+#    #+#             */
-/*   Updated: 2025/10/22 14:37:35 by mguilber         ###   ########.fr       */
+/*   Updated: 2025/10/24 17:39:29 by mguilber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int st(char *s1, char *set, int i){
+	int len = ft_strlen(set);
+	int j = 0;
+	
+	while(set[j] || j == len + 1){
+			if(s1[i] == set[j]){
+				i++;
+				j = 0;
+			}
+			else
+				j++;
+	}
+	return(i);
+}
+
+int end(char *s1, char *set, int i){
+	int len = ft_strlen(set);
+	int im = ft_strlen(s1);
+	int j = 0;
+
+	while((set[j] || j == len + 1) && im >= i){
+		if(s1[im - 1] == set[j]){
+			im--;
+			j = 0;
+		}
+		else
+			j++;
+	}
+	return(im);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		j;
-	int		i;
-	int		x;
-	int		v;
-	char	*str;
+	int i = 0;
+	char *str;
+	int v = 0;
+	int im;	
 
-	j = 0;
-	i = 0;
-	x = 0;
-	v = 0;
-	str = malloc((ft_strlen(s1) + 1) * sizeof(char));
-	while (s1[i])
-	{
-		while (set[j])
-		{
-			if (s1[i] == set[j])
-				v++;
-			else if (s1[i] != set[j] && v == 0)
-			{
-				str[x] = s1[i];
-			}
-			j++;
-		}
-		if (v == 0)
-			x++;
-		v = 0;
-		j = 0;
+	
+	i = st((char *)s1, (char *)set, i);
+	im = end((char *)s1, (char *)set, i);
+	str = malloc(im - i +1);
+	if(!str)
+		return (NULL);
+	while(s1[i] && i < im){
+		str[v] = s1[i];
 		i++;
+		v++;
 	}
-	str[x] = '\0';
-	return (str);
+	str[v] = '\0';
+	return(str);
 }
 /*
-int	main(int argc, char const *argv[]) {
-  printf("%s\n", ft_strtrim("hello world", "hod"));
-  return (0);
-}
-*/
+#include <stdio.h>
+int main()
+{
+char *s1 = "  \t \t \n bonjour  \n\n\n\t";
+ 	char *s2 = "bonjour";
+ 	char *ret = ft_strtrim(s1, " \n\t");
+ 
+ 	if (!strcmp(ret, s2))
+ 		printf("suuces");
+ 	printf("connard");
+ return(0);
+
+}*/
